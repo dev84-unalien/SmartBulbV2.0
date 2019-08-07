94c49f9fef8cd75b46770a93d046c9a81310a3b4
paul <idelec@free.fr>
2019-08-07 14:44:08 +0200

    version finale SmartBulb

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
-
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
+import { NavController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.page.html',
-  styleUrls: ['./sign-up.page.scss']
+  selector: "app-sign-up",
+  templateUrl: "./sign-up.page.html",
+  styleUrls: ["./sign-up.page.scss"]
 })
 export class SignUpPage implements OnInit {
-  constructor() {}
+  constructor(public nav: NavController) {}
 
   ngOnInit() {}
 
   showInput() {
-    let value = $('#email').val();
-    let password = $('#password').val();
+    let value = $("#email").val();
+    let password = $("#password").val();
     alert(value);
     alert(password);
+    this.nav.navigateForward("bulb");
   }
 }
